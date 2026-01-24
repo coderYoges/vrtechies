62023aeaf838d9d4018e3d9353e01254b993fe0c
Coder Yoges <rvyogeswaran@gmail.com>
2026-01-24 17:19:48 +0530

    chore: deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +10,6 @@ export default defineConfig({
       },
     }),
   ],
-})
+  // Replace 'repo-name' with your actual GitHub repository name
+  base: '/vrtechies/',
+});
